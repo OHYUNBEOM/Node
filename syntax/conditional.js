30acc79061a3aa75421ec3fb4944585f14ee2f2c
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-06-13 01:00:25 +0900

    modifying

--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,10 +1,13 @@
+var args=process.argv;
+//사용자에게 입력받는값
+console.log(args[2]);
 console.log('A');
 console.log('B');
-if(true)
+if(args[2]==='1')//1이면 true 로 판단/ C1 출력
 {
     console.log('C1');
 }
-else
+else// 그 외 false로 판단/ C2 출력
 {
     console.log('C2');
 }
