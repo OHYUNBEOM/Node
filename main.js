19cec4a030c40530e3a1be9a1c884034ba273e3b
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-06-14 00:24:51 +0900

    not found

--- main.js
+++ main.js
@@ -5,35 +5,37 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var title = queryData.id;
-    if(_url == '/'){
-        title = 'Welcome';
-        queryData.id='Firstpage';
+    var pathname=url.parse(_url,true).pathname;
+    if(pathname==='/')//root page 접속
+    {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var template = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ul>
+                <h2>${title}</h2>
+                <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
     }
-    if(_url == '/favicon.ico'){
-        return response.writeHead(404);
+    else
+    {
+        response.writeHead(404);
+        response.end('Not found');
     }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-    var template = `
-        <!doctype html>
-        <html>
-        <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.end(template);
-    })
 });
 app.listen(3000);
