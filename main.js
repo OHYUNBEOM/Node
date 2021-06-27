a4051ed42eb991214add4f470eb443833992b4ba
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-06-28 00:22:22 +0900

    fix code with function

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+function templateHTML(title, list,body)//코드 재사용
+{
+    return `
+    <!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list} 
+        ${body}
+    </body>
+    </html>
+    `;
+}
+function templatelist(filelist)
+{
+    var list='<ul>';
+    var i=0;
+    while(i < filelist.length){
+        list= list+`<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;//list 목록에 filelist에 해당되는것 추가
+        i=i+1;
+    }
+    list=list+'</ul>';
+    return list;
+}
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -12,29 +40,9 @@ var app = http.createServer(function(request,response){
             fs.readdir('./data',function(error, filelist){
             var title='Welcome';
             var description = 'Hello, Node.js';
-            var list='<ul>';
-            var i=0;
-            while(i < filelist.length){
-                list= list+`<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;//list 목록에 filelist에 해당되는것 추가
-                i=i+1;
-            }
-
-            list=list+'</ul>';
-            var template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list} 
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                </body>
-                </html>
-                `;
+            var list = templatelist(filelist);
+            var template = templateHTML(title,list,`<h2>${title}</h2>${description}`); // 동일한 코드를 함수화
+            //templateHTML 함수를 호출하는데 내가 지정해둔 title,list를 순차적으로 가져오고 크게 잡은 body 태그부분이 title 과 그에 해당하는 description 을 불러옴
                 response.writeHead(200);
                 response.end(template);
                 // 다음과 같은 작업을 통해서 data file 에 file 이 추가될 때 마다 알아서 원래의 형식에 맞도록 업로드가 되고 사용자가 수정해줄 필요가 없어짐
@@ -43,33 +51,10 @@ var app = http.createServer(function(request,response){
         else
         {
             fs.readdir('./data',function(error, filelist){
-                var title='Welcome';
-                var description = 'Hello, Node.js';
-                var list='<ul>';
-                var i=0;
-                while(i < filelist.length){
-                    list= list+`<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;//list 목록에 filelist에 해당되는것 추가
-                    i=i+1;
-                }
-                list=list+'</ul>';
-
             fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
                 var title = queryData.id;
-                var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                        <title>WEB1 - ${title}</title>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                    </body>
-                    </html>
-                    `;
+                var list = templatelist(filelist);
+                var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
                     response.writeHead(200);
                     response.end(template);
                 });
